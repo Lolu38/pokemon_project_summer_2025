df63e83de76ee6cbbee147f7a04cdf01806587ef
Lolu38 <chiappetta.lucas38@gmail.com>
2025-07-12 11:31:53 +0200

    Add poekmon class

---
+++ python_file/class_pokemon.py
@@ -0,0 +1,12 @@
+class Pokemon:
+    def __init__(self, name, level, type_):
+        self.name = name
+        self.level = level
+        self.type_ = type_
+
+    def __str__(self):
+        return f"{self.name} (Level {self.level}, Type: {self.type_})"
+
+    def level_up(self):
+        self.level += 1
+        print(f"{self.name} leveled up to Level {self.level}!")
