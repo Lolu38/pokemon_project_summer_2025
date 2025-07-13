cf0bb10b57335bf4a1fb624448ef4181276da44f
Lolu38 <chiappetta.lucas38@gmail.com>
2025-07-13 16:08:35 +0200

    Start to edit pokemon

---
+++ python_file/edit_pokemon.py
@@ -0,0 +1,61 @@
+import class_pokemon
+import sqlite3
+
+file = "./data_base/box_pokemon.db"
+link = sqlite3.connect(file)
+cursors = link.cursor()
+
+def creation_table():
+    cursors.execute('''
+        CREATE TABLE IF NOT EXISTS Pokemon (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE NOT NULL,
+            hp INT NOT NULL,
+            attack INT NOT NULL
+            defense INT NOT NULL,
+            speed INT NOT NULL,
+            type_ TEXT NOT NULL,
+            attack1 INT NOT NULL,  
+            attack2 INT NOT NULL,  
+            attack3 INT NOT NULL,  
+            attack4 INT NOT NULL,  
+            precision FLOAT NOT NULL,
+            esquive FLOAT NOT NULL
+        )
+    ''') #We have to puts the attacks as integers, because we will use the id of the attack in the attack_set
+    link.commit()
+
+
+def create_pokemon(name, hp, attack, defense, speed, type_,  attack_set, precision, esquive):
+    creation_table()
+
+    try:
+        cursors.execute('''
+            INSERT INTO pokemon (name, hp, attack, defense, speed, type_,  attack_set, precision, esquive)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', (name, hp, attack, defense, speed, type_,  attack_set, precision, esquive))
+        
+        link.commit()
+        print(f"✅ Pokémon '{name}' créé avec succès !")
+    
+    except sqlite3.IntegrityError:
+        print(f"⚠️ Le nom de pokémon '{name}' existe déjà. Choisis-en un autre.")
+    
+    finally:
+        link.close()
+
+
+def edit_pokemon(name):
+    creation_table()
+
+    cursors.execute('''
+        SELECT * FROM pokemon WHERE name = ?
+    ''', (name))
+    result = cursors.fetchone()
+    if not result:
+        print(f"⚠️ Aucun Pokémon trouvé avec le nom '{name}'.")
+        return
+
+    
+    print(f"✅ Pokémon '{name}' mis à jour avec succès !")
+    
+    link.close()
---
+++ python_file/sql_users_python.py
@@ -0,0 +1,90 @@
+import sqlite3
+
+file = "./data_base/box_users.db"
+link = sqlite3.connect(file)
+cursors = link.cursor()
+
+def creation_table():
+    cursors.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role TEXT NOT NULL
+        )
+    ''')
+    link.commit()
+    #print("✅ Table 'users' créée ou déjà existante.")
+
+
+def handle_user(action):
+    username = input("Choisis un nom d'utilisateur : ")
+    password = input("Choisis un mot de passe : ")
+
+    if action == "login":
+        connect_user(username, password)
+    elif action == "register":
+        register_user(username, password)
+    else:
+        print("⚠️ Action non reconnue. Choisissez 'login' o u 'register'.")
+
+
+def register_user(username, password):
+    creation_table()
+    try:
+        cursors.execute('''
+            INSERT INTO users (username, password, role)
+            VALUES (?, ?, ?)''', (username, password, "player"))
+        
+        link.commit()
+        print(f"✅ Utilisateur '{username}' créé avec succès !")
+    
+    except sqlite3.IntegrityError:
+        print(f"⚠️ Le nom d'utilisateur '{username}' existe déjà. Choisis-en un autre.")
+    
+    finally:
+        link.close()  
+
+
+def connect_user(username, password):
+    creation_table()
+    cursors.execute('''
+        SELECT * FROM users WHERE username = ? AND password = ?
+    ''', (username, password))
+    
+    user = cursors.fetchone()
+    
+    if user:
+        print(f"✅ Connexion réussie pour l'utilisateur '{username}' !")
+    else:
+        print("⚠️ Nom d'utilisateur ou mot de passe incorrect.")
+    
+    link.close()
+
+def who_is_connected():
+    creation_table()
+    cursors.execute('''
+        SELECT username FROM users
+    ''')
+    
+    users = cursors.fetchall()
+    
+    if users:
+        print("Utilisateurs connectés :")
+        for user in users:
+            print(f"- {user[0]}")
+    else:
+        print("Aucun utilisateur connecté.")
+    
+    link.close()
+
+
+
+
+
+
+
+def __main__():
+    handle_user(action=input("Choisissez une action (login/register): ").strip().lower())
+    who_is_connected()
+__main__()
