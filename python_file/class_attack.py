df0531e90f856f9ee5016bbed0c66bfe67131f0d
Lolu38 <chiappetta.lucas38@gmail.com>
2025-07-13 15:28:30 +0200

    Plans changed: no more connexion

--- python_file/class_attack.py
+++ python_file/class_attack.py
@@ -1,8 +1,9 @@
 class Attack:
-    def __init__(self, name, power, type_):
+    def __init__(self, name, power, type_, nbr_uses):
         self.name = name
         self.power = power
         self.type_ = type_
+        self.nbr_uses = nbr_uses
 
     def __str__(self):
         return f"{self.name} (Power: {self.power}, Type: {self.type_})"
@@ -15,3 +16,6 @@ class Attack:
 
     def get_name(self):
         return self.name
+
+    def get_nbr_uses(self):
+        return self.nbr_uses
--- python_file/class_pokemon.py
+++ python_file/class_pokemon.py
@@ -1,16 +1,36 @@
 class Pokemon:
-    def __init__(self, name, level, type_):
+    def __init__(self, name, hp, attack, defense, speed, type_,  attack_set, precision, esquive):
         self.name = name
         self.type_ = type_
-        self.hp = level * 10
-        self.attack = level * 2
-        self.defense = level * 2
-        self.speed = level * 2
-        self.attacks = []
-        self.precision = 0.9
-        self.esquive = 0.1
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+        self.attack_set = []
+        self.precision = precision
+        self.esquive = esquive
         
     def __str__(self):
         return f"{self.name} (Type: {self.type_})"
 
+    def get_name(self):
+        return self.name
 
+    def get_type(self):
+        return self.type_
+
+    def get_hp(self):
+        return self.hp
+
+    def get_attack(self):
+        return self.attack
+
+    def get_defense(self):
+        return self.defense
+
+    def get_speed(self):
+        return self.speed
+
+    def get_attack_set(self):
+        return self.attack_set
+    
